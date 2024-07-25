abbac236b7055ef55ff0c3bb1eb926a06f5e8433
Pranaya Shrestha <shresthapranaya2018@gmail.com>
2024-07-25 10:16:22 +0545

    javascript
    Something

---
+++ pranaya/js/js.js
@@ -0,0 +1,468 @@
+<!doctype html>
+!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pranaya Shrestha</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="Z:\New folder\Resume\css\nikolai.css">
+     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+     <link rel="stylesheet" type="text/css" href ="Z:\New folder\Resume\css\owl.carousel.css">
+
+
+     <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
+  </head>
+  <body>
+  	
+  	
+<!-- navbar -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+  	<i class="fa-solid fa-dragon"></i>
+    <a class="navbar-brand" href="#">Pranaya Shrestha</a>
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link dropdown-toggle" aria-current="page" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">About me</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+       <li class="nav-item">
+          <a class="nav-link" href="#">Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dropdown-toggle" aria-current="page" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Portfolio</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link dropdown-toggle" aria-current="page" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Get it now</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+<!-- navbar ends -->
+<!-- about me starts-->
+
+
+<div class="container abt-me-sec">
+    <div class="row">
+       <div class="col-md-5">
+        
+     <div class="abt-me-image">
+        <img class="image fluid" src="Z:\New folder\Resume\images\sigma.jpg">
+      </div>
+    </div>
+    <div class="col-md-7">
+        <div class="abt-me-head">
+          <h6>
+            Frontend Freelance-developer
+            </h6>
+            </div>
+            <h1>
+              Pranaya Shrestha
+            </h1>
+            <h6 class="nikolai-head">
+             These beaches are popular for recreation, playing important economic and cultural roles—often driving local tourism industries. To support these uses, some beaches have human-made infrastructure, such as lifeguard posts, changing rooms, showers, shacks and bars.
+            </h6>
+            <div>
+
+              <div> 
+               <a href="Z:\New folder\Resume\index.html">
+              <button type="button" class="btn nikolai-download-button">
+                Download CV
+              </button>
+              </a>
+
+              <a href="Z:\New folder\Resume\index.html">
+               <button type="button" class="btn nikolai-contact-button">
+                Contact
+               </button>
+              </a>
+               
+             </div>
+            </div> 
+    </div>
+</div>
+
+<!-- what i do -->
+<div class="container">
+	<div class="test">
+		<p>
+		What do i do?
+	</p>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-1">
+			<div class="wild-icon">
+		<i class="fa-solid fa-pencil"></i>
+	</div>
+	</div>
+
+
+
+
+	<div class="col-md-5">
+		<h4>
+			Copywrite
+		</h4>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit sodales iaculis. Aenean lacinia vel sapien sit amet sollicitudin. Curabitur at faucibus magna. Aenean eget dignissim nulla. Duis enim elit, fringilla sed dapibus sed, tristique vitae urna. Donec gravida consectetur velit, et ullamcorper dolor suscipit nec. Nulla facilisi. Suspendisse potenti.
+		</p>
+	</div>	
+	
+		<div class="col-md-1">
+			<div class="wild-icon">
+        <i class="fa-solid fa-shop"></i>
+	</div>
+	</div>
+
+
+
+
+	<div class="col-md-5">
+		<h4>
+			E-Commerce
+		</h4>
+		<p>
+			Ut eget quam dui. Sed non lorem non arcu mattis aliquam. In quis elit eu est accumsan suscipit. Aliquam a turpis non diam hendrerit consectetur vitae vitae sapien. Nam ut arcu et tellus tincidunt molestie vel in lorem. Pellentesque nec arcu convallis, vestibulum nisi eget.
+		</p>
+	</div>	
+
+
+
+	</div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-1">
+      <div class="wild-icon">
+   <i class="fa-solid fa-computer"></i>
+  </div>
+  </div>
+
+
+
+
+  <div class="col-md-5">
+    <h4>
+      Web-Designing
+    </h4>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet nisl nibh, ac molestie diam ullamcorper at. In eu ex in felis auctor pulvinar sed vitae diam. Integer maximus enim accumsan, consectetur ante ac, lobortis justo. Pellentesque ultricies velit tempor lacus faucibus placerat. Donec ut lectus pulvinar, vestibulum sem in, elementum augue. Vivamus metus neque, pretium scelerisque libero vitae, congue.
+    </p>
+  </div>  
+  
+    <div class="col-md-1">
+      <div class="wild-icon">
+        <i class="fa-solid fa-bullhorn"></i>
+  </div>
+  </div>
+
+
+
+
+  <div class="col-md-5">
+    <h4>
+      Marketting
+    </h4>
+    <p>
+     Praesent fermentum dolor a lorem tristique, at sodales justo posuere. In lacinia, urna eu ultricies consequat, felis mauris finibus mi, id hendrerit justo leo ut lacus. Pellentesque consequat lacus fermentum facilisis luctus. Vestibulum vitae condimentum massa. Pellentesque pellentesque posuere vehicula. Proin ultricies, est vel sagittis sollicitudin, augue ligula pellentesque nisl, non efficitur risus erat id.
+    </p>
+  </div>  
+
+
+
+  </div>
+</div>
+
+
+
+<!-- testimonials -->
+<div class="container">
+  <div class="test">
+    <p>
+  Testimonials
+  </p>
+  </div>
+</div>
+
+<div class="container">
+<div class="owl-carousel owl-theme">
+    <div class="item">
+    	<div class="container anime-border">
+			<div class="row ">
+				<div class="col-md-2">
+					<img src="Z:\New folder\Resume\images\sigma.jpg" class="image">
+				</div>
+				<div class="col-md-5">
+					<p>
+					popop opopopopo popopopo opppopoopoo poppoppppphgpofhi
+					</p> 
+				</div>
+			</div>
+		</div>
+	</div>	
+	<div class="item">
+    	<div class="container anime-border">
+			<div class="row ">
+				<div class="col-md-2">
+					<img src="Z:\New folder\Resume\images\face.jpg" class="image">
+				</div>
+				<div class="col-md-5">
+					<p>
+					zack the baker
+					</p> 
+				</div>
+			</div>
+		</div>
+	</div>	
+	<div class="item">
+    	<div class="container anime-border">
+			<div class="row ">
+				<div class="col-md-2">
+					<img src="Z:\New folder\Resume\images\black.jpg" class="image">
+				</div>
+				<div class="col-md-5">
+					<p>
+					wassup
+					</p> 
+				</div>
+			</div>
+		</div>
+	</div>	
+  
+</div>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js\owl.carousel.js">
+    </script>
+    <script type="text/javascript">
+    	
+    	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+    </script>
+
+
+    
+
+
+
+<!-- Clients -->
+<div class="container">
+  <div class="test">
+    <p>
+  Clients
+  </p>
+  </div>
+</div>
+
+
+
+<div class="container">
+<div class="owl-carousel owl-theme">
+    <div class="item">
+      <a href="Z:\New folder\Resume\index.html">
+      <img src="Z:\New folder\Resume\images\client-2.png" class="img-fluid"> 
+      </a>
+    </div>
+    <div class="item">
+      <a href="Z:\New folder\Resume\index.html">
+        <img src="Z:\New folder\Resume\images\client-7.png" class="img-fluid">
+      </a>
+    </div>
+    <div class="item">
+      <a href="Z:\New folder\Resume\index.html">
+      <img src="Z:\New folder\Resume\images\Magic-Furnitures-1.png" class="img-fluid">
+      </a>
+    </div>
+    <div class="item">
+      <a href="Z:\New folder\Resume\index.html">
+        <img src="Z:\New folder\Resume\images\sweety.png" class="img-fluid">
+      </a>
+    </div>
+    <div class="item">
+      <a href="Z:\New folder\Resume\index.html">
+        <img src="Z:\New folder\Resume\images\designlovers.png" class="img-fluid">
+      </a>
+    </div>
+    <div class="item">
+      <a href="Z:\New folder\Resume\index.html">
+        <img src="Z:\New folder\Resume\images\lifeguard.png" class="img-fluid">
+      </a>
+    </div>
+    <div class="item">
+      <a href="Z:\New folder\Resume\index.html">
+        <img src= "Z:\New folder\Resume\images\goldengrid.png" class="img-fluid">
+      </a>
+    </div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js\owl.carousel.js">
+    </script>
+    <script type="text/javascript">
+    	
+    	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+    </script>
+
+
+<!-- Fun facts -->
+<div class="container">
+  <div class="test">
+    <p>
+ Fun facts 
+  </p>
+  </div>
+</div>
+
+
+
+<div class=""container">
+<div class="owl-carousel owl-theme">
+    <div class="item">
+    	<div class="container anime-border">
+			<div class="row ">
+				<i class="fa-regular fa-heart"></i>
+				<div class="col-md-5">
+
+				</div>
+			</div>
+		</div> 
+	</div>
+    <div class="item">    	
+    	<div class="container anime-border">
+			<div class="row ">
+
+				<div class="col-md-5">
+					<i class="fa-regular fa-clock"></i>
+				</div>
+			</div>
+		</div> 
+	</div>
+    <div class="item">    	
+    	<div class="container anime-border">
+			<div class="row ">
+				<i class="fa-solid fa-star"></i>
+				<div class="col-md-5">
+
+				</div>
+			</div>
+		</div> 
+	</div>
+    <div class="item">  	
+    	<div class="container anime-border">
+			<div class="row ">
+				<i class="fa-solid fa-mug-hot"></i>
+				<div class="col-md-5">
+
+				</div>
+			</div>
+		</div> 
+	</div>
+</div>
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js\owl.carousel.js">
+    </script>
+    <script type="text/javascript">
+    	
+    	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+    </script>
+
+
+  </body>
+</html>
+
